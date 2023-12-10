8cdf85f2eb878d3cf9f4f274189aeaf71c439d0f
Nurbek Alisherov <134116375+alisherovuz@users.noreply.github.com>
2023-12-10 13:01:19 +0500

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -19,7 +19,7 @@ export default function Home({ posts }) {
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          {!posts.length && 'No posts found.'}
+          {!posts.length && 'Postlar topilmadi.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
             const { slug, date, title, summary, tags } = post
             return (
@@ -57,7 +57,7 @@ export default function Home({ posts }) {
                         <Link
                           href={`/blog/${slug}`}
                           className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`Read "${title}"`}
+                          aria-label={`o'qish "${title}"`}
                         >
                           Read more &rarr;
                         </Link>
